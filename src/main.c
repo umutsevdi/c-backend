32c951bd4565a4d2b319c450405689daf0343379
Umut Sevdi <sevdiumut@gmail.com>
2023-05-28 02:39:57 +0300

    fixed memory leak

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
     g_set_application_name("My Web Server");
     GHashTable* table = util_parse_args(argc, argv);
     g_hash_table_foreach(table, print_key_values, NULL);
-
+    g_hash_table_destroy(table);
     struct MHD_Daemon* daemon;
     daemon = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION, 8080,
         NULL, NULL, &request_handler, NULL, MHD_OPTION_END);
@@ -29,8 +29,6 @@ int main(int argc, char* argv[])
     printf("Server running on port 8080...\n");
     getchar(); // wait for a key to stop server
     MHD_stop_daemon(daemon);
-
-    g_hash_table_destroy(table);
     return 0;
 }
 
@@ -54,14 +52,15 @@ enum MHD_Result request_handler(void* cls, struct MHD_Connection* connection,
         MHD_RESPMEM_PERSISTENT);
 
     GString* mime = util_get_mimetype(url);
-    printf("%s\n", mime->str);
 
     MHD_add_response_header(http_response, "Content-Type", mime->str);
-    MHD_add_response_header(http_response, "Content-Length", g_strdup_printf("%zu", g_response->len));
+    gchar* content_length = g_strdup_printf("%zu", g_response->len);
+    MHD_add_response_header(http_response, "Content-Length", content_length);
 
     int ret = MHD_queue_response(connection, MHD_HTTP_OK, http_response);
     MHD_destroy_response(http_response);
     g_string_free(mime, TRUE);
+    g_free(content_length);
     /*whether created on stack or not*/
     g_string_free(g_response, FALSE);
     return ret;
